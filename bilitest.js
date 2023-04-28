afcee12d57d85fd90039d42d176638572e27db12
Mastiff <mastiffzone365@gmail.com>
2023-04-28 11:07:36 +0800

    1

--- bilitest.js
+++ bilitest.js
@@ -3,6 +3,5 @@ delete bodyOri.data.live_tip;
 delete bodyOri.data.senior_gate
 delete bodyOri.data.vip_section
 delete bodyOri.data.vip_section_v2
-$notification.post(typeof(bodyOri), "a", bodyOri);
 console.log("done")
 $done({body: JSON.stringify(bodyOri)});
